45c36376bd7de9a2f6a1631499c4b13bc4dab2b1
Shadow-of-Diamond <xzk1072503@gmail.com>
2019-08-23 16:25:15 +0800

    test

--- my_test/hsrouter.py
+++ my_test/hsrouter.py
@@ -1,39 +1,36 @@
 #coding:UTF-8
-import requests,sys
+import requests
+import re
 
 login_url = 'http://192.168.1.209:8888/admin/login.pl'
 arp_url = 'http://192.168.1.209:8888/admin/info_arpcache.pl'
+keywords = 'class|align|href'
+ip_list = []
+mac_list = []
+arp_list = []
 
-#imp.reload(sys)
-session = requests.session()
-headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 SE 2.X MetaSr 1.0"}
-data = {"loginid":"xiezhenkun", "passwd":"29ab20c716a35cb80722617ee355227c","save":"on","ok":"登录"}
-session.post(login_url, data = data)
-response = session.get(arp_url)
+def hsroute():
+    session = requests.session()
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 SE 2.X MetaSr 1.0"}
+    data = {"loginid": "xiezhenkun","passwd": "29ab20c716a35cb80722617ee355227c", "save": "on", "ok": "登录"}
+    session.post(login_url, data=data)
+    response = session.get(arp_url)
+    keywords = 'class|align|href'
+    data = response.text
+    for line in data.split('\n'):
+        if re.search(keywords, line) == None and len(line.split('<td>')) == 2 and re.search('</td>', line) != None and re.search('<td>\w+<\/td>', line) == None:
+            newline = line.split('<td>')[1].split('</td>')[0].replace('&nbsp;', '').strip()
+            #print (newline)
+            if re.search('-', newline) == None:
+                ip_list.append(newline)
+            else:
+                mac_list.append(newline)
+    if len(ip_list) == len(mac_list):
+        for i in range(len(ip_list)):
+            arp_list.append(ip_list[i] + '\t\t' + mac_list[i])
+            print(arp_list)
 
-
-
-temp1 = 'C:\\Users\\Administrator\\Desktop\\temp1.txt'
-temp2 = 'C:\\Users\\Administrator\\Desktop\\temp2.txt'
-w1 = open(temp1,'w')
-w1.write(str(response.text.encode('utf8')))
-w1.close()
-print (response.text)
-r1 = open(temp1,'r')
-w2 = open(temp2,'w')
-for line in r1.readlines():
-    if re.search(keywords,line) == None and len(line.split('<td>')) == 2 and re.search('</td>',line) != None and re.search('<td>\w+<\/td>',line) == None:
-        newline = line.split('<td>')[1].split('</td>')[0].replace('&nbsp;','')
-        if re.search('-',newline) == None:
-            w2.write(newline + '\t\t')
-        else:
-            w2.write(newline + '\n')
-w2.close()
-
-
-
+hsroute()
 
 
 
-for i in temp1,temp2:
-    os.remove(i)
