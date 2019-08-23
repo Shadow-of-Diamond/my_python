b152b23599ce1249bada1a2cc31035cbffc146b2
Shadow-of-Diamond <xzk1072503@gmail.com>
2019-08-23 16:29:14 +0800

    test

--- my_test/sendmail.py
+++ my_test/sendmail.py
@@ -5,7 +5,7 @@ from email.mime.text import MIMEText
 #设置服务器所需信息
 mail_host = 'smtp.163.com'  # 163邮箱服务器地址
 mail_user = 'xzk1072503@163.com'  # 163用户名
-mail_pass = 'xzK343206603'  # 密码(部分邮箱为授权码。此处即为授权码)
+mail_pass = 'xzK3....'  # 密码(部分邮箱为授权码。此处即为授权码)
 sender = 'xzk1072503@163.com'  # 邮件发送方邮箱地址
 receivers = ['xiezhenkun@pconline.com.cn', '251845857@qq.com'] # 收件箱地址
 
